0e475fe3c1b36d9fd0c11ff2d26b688307316a78
qkr10 <qkrtngks10@naver.com>
2023-09-18 13:57:44 +0900

    기능수정: 전역변수를 개발자 도구에서도 쓸수있게 수정

--- src/globalVariables.js
+++ src/globalVariables.js
@@ -1,9 +1,11 @@
 export let editor;
 export function setEditor(e) {
+    window.editor = e;
     editor = e;
 }
 
 export let dragStart;
 export function setDragStart(d) {
+    window.dragStart = dragStart
     dragStart = d;
 }
